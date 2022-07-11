47779490a15292c6d7413a8b805f9df31ee9b3fb
DmitriiDobr <72938753+DmitriiDobr@users.noreply.github.com>
2022-07-11 20:58:05 +1100

    fixes

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,9 +1,36 @@
 from django.contrib import admin
-
+from django.core.exceptions import ValidationError
 from .models import Article, Tag, TagArticleRelation
+from django.forms import BaseInlineFormSet
+
+
+class AboutScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        selected_topics = 0
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            # form.cleaned_data
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+            # raise ValidationError('Тут всегда ошибка')
+
+            if form.cleaned_data and form.cleaned_data['main']:
+                selected_topics += 1
+
+        if selected_topics == 0:
+            raise ValidationError('Выберите основной раздел статьи')
+        if selected_topics > 1:
+            raise ValidationError('Основной раздел может быть только один')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
 
 class ArticleTagInline(admin.TabularInline):
     model = TagArticleRelation
+    formset = AboutScopeInlineFormset
+
 
 
 @admin.register(Tag)
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Tag(models.Model):
-    tag = models.CharField(max_length=50)
+    tag = models.CharField(max_length=50, verbose_name="Раздел")
+
+    class Meta:
+        verbose_name = "Раздел"
 
     def __str__(self):
         return self.tag
@@ -14,7 +17,9 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-    tags = models.ManyToManyField(Tag,blank=True,verbose_name='Разделы')
+    tags = models.ManyToManyField(Tag,blank=True,
+                                  verbose_name='Разделы',
+                                  through='TagArticleRelation')
 
 
     class Meta:
@@ -26,6 +31,6 @@ class Article(models.Model):
 
 
 class TagArticleRelation(models.Model):
-    cur_tag = models.ForeignKey(Tag,verbose_name='раздел',on_delete=models.CASCADE)
-    articles = models.ForeignKey(Article, verbose_name='статья',on_delete=models.CASCADE)
+    cur_tag = models.ForeignKey(Tag,verbose_name='раздел',on_delete=models.CASCADE, related_name='scopes')
+    articles = models.ForeignKey(Article, verbose_name='статья',on_delete=models.CASCADE, related_name='scopes')
     main = models.BooleanField(default=False)
